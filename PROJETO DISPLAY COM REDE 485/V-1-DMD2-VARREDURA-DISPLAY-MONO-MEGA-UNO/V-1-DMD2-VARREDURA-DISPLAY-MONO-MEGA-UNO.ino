a6d2d6b665a5c6d4e76418a91f42c04f0341b85a
oinetsrv <oinetsrv@gmail.com>
2021-09-10 17:10:12 -0300

    Organização de código

--- PROJETO DISPLAY COM REDE 485/V-1-DMD2-VARREDURA-DISPLAY-MONO-MEGA-UNO/V-1-DMD2-VARREDURA-DISPLAY-MONO-MEGA-UNO.ino
+++ PROJETO DISPLAY COM REDE 485/V-1-DMD2-VARREDURA-DISPLAY-MONO-MEGA-UNO/V-1-DMD2-VARREDURA-DISPLAY-MONO-MEGA-UNO.ino
@@ -1,19 +1,42 @@
 /*
-   PROJETO  : Controle de DISPLAY 
-   AUTOR    : STENIO RODRIGUES
-   DATA     : 09/08/2021
-   OBJETIVO :
-              Imprimir mensagens recebidas via rede 485
------------------------------------------------------------
-Mapeamento hardware
-________________________
+ ----------------------------------------------------------
+|   PROJETO  : Controle de display via rede 485            |
+|   AUTOR    : STENIO RODRIGUES                            |
+|   DATA     : 09/08/2021                                  |
+|   OBJETIVO :                                             |
+|              Imprimir mensagens recebidas via rede 485   |
+|              aplicação industria de fertilizantes        |
+ ----------------------------------------------------------
+      Organizando código.
+    // =================================================================================
+    // --- Bibliotecas Auxiliares       --- //
+    // =================================================================================
+    // --- Mapeamento de Hardware       --- //
+    // =================================================================================
+    // --- Instâncias                   --- //
+    // =================================================================================
+    // --- Protótipo das Funções        --- //
+    // =================================================================================
+    // --- Variáveis Globais            --- //
+    // =================================================================================
+    // --- Interrupções                 --- //
+    // =================================================================================
+    // --- Configurações Iniciais SETUP --- //
+    // =================================================================================
+    // --- Loop Infinito                --- //
+    // =================================================================================
+    // --- Desenvolvimento das Funções  --- //
+    // =================================================================================
+
+   Mapeamento hardware
+ -----------------------
 |1 pin_noe 2  pin_a     |
 |3  GND    4  pin_b     |
 |5  GND    6  N/C       |
 |7  GND    8  pin_sck   |
 |9  GND    10 pin_clk   |
 |11 GND    12 pin_r_data|
-________________________
+ -----------------------
 // PAINEL           MEGA
 //byte panelsWide   N/C
 //byte panelsHigh   N/C
@@ -25,18 +48,6 @@ ________________________
 //byte pin_clk      8
 //byte pin_r_data   51
 
-*/
-
-#include <SPI.h>
-#include <DMD2.h>
-#include <fonts/Arial14.h>
-
-const int COUNTDOWN_FROM = 12;
-int counter = COUNTDOWN_FROM;
-int QDISPLAY = 1;
-
-SoftDMD dmd(QDISPLAY, 1, 9, 6, 7, 8, 52, 51); // MEGA DMD controls the entire display
-//SoftDMD dmd(QDISPLAY, 1, 9, 6, 7, 8, 12, 11); // UNO
 // PAINEL           MEGA      UNO
 //byte panelsWide   N/C
 //byte panelsHigh   N/C
@@ -46,24 +57,39 @@ SoftDMD dmd(QDISPLAY, 1, 9, 6, 7, 8, 52, 51); // MEGA DMD controls the entire di
 //byte pin_b        7         7
 //byte pin_sck      52        12    
 //byte pin_clk      8         8
-//byte pin_r_data   51        11
+//byte pin_r_data   51        11        */
 
-DMD_TextBox box (dmd,  1, 2);  // "box" provides a text box to automatically write to/scroll the display
-DMD_TextBox box1(dmd, 31, 2);
-DMD_TextBox box2(dmd, 63, 2);
-DMD_TextBox box3(dmd, 95, 2);
-
+// --- Bibliotecas Auxiliares       --- //
+#include <SPI.h>
+#include <DMD2.h>
+#include <fonts/Arial14.h>
+// =================================================================================
+// --- Variáveis Globais            --- //
+const int COUNTDOWN_FROM = 12;
+int counter = COUNTDOWN_FROM;
+int QDISPLAY = 1;
+int tam_msg = 6;
+// --- Instâncias                   --- //
+SoftDMD dmd(QDISPLAY, 1, 9, 6, 7, 8, 52, 51); // MEGA DMD controls the entire display
+//SoftDMD dmd(QDISPLAY, 1, 9, 6, 7, 8, 12, 11); // UNO
+DMD_TextBox box(dmd, 1, 2); // "box" provides a text box to automatically write to/scroll the display
+//DMD_TextBox box1(dmd, 31, 2);
+//DMD_TextBox box2(dmd, 63, 2);
+//DMD_TextBox box3(dmd, 95, 2);
+// =================================================================================
+// --- Configurações Iniciais SETUP --- //
 void setup() {
-  Serial.begin(9600); 
-  dmd.setBrightness(255);
-  dmd.selectFont(Arial14);
-  dmd.begin();
+    Serial.begin(9600);
+    dmd.setBrightness(255);
+    dmd.selectFont(Arial14);
+    dmd.begin();
     Serial.println("Projeto controle rede de displays fabrica de fertilizantes.\n");
     Serial.println("                                                             ");
     Serial.println("Nome arquivo:V-1-DMD2-VARREDURA-DISPLAY-MONO-MEGA.ino... \n");
     
-}// end setup
-
+} // end setup
+// =================================================================================
+// --- Loop Infinito                --- //
 void loop() {
   Serial.print(counter);
   Serial.println(F("..."));
@@ -84,3 +110,5 @@ void loop() {
     delay(1000);
   }// end if(counter == 0)
 }// end loop
+// =================================================================================
+
